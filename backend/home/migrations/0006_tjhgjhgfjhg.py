8cccea82b09309706adf2e14b2749c60a90ef717
Crowdbotics <team@crowdbotics.com>
2020-07-09 13:31:30 +0000

    Update models

--- backend/home/migrations/0006_tjhgjhgfjhg.py
+++ backend/home/migrations/0006_tjhgjhgfjhg.py
@@ -6,15 +6,23 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0005_hgffjhfjhg'),
+        ("home", "0005_hgffjhfjhg"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Tjhgjhgfjhg',
+            name="Tjhgjhgfjhg",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('mhkjhkjhk', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("mhkjhkjhk", models.BigIntegerField()),
             ],
         ),
     ]
--- backend/home/models.py
+++ backend/home/models.py
@@ -8,6 +8,7 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    ghhfhfhg = models.SlugField(max_length=50, null=True, blank=True,)
 
     def __str__(self):
         return self.title
